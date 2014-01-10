b0da59a4cc9ebf6d43324bf3621f8a738426b858
Peter Goulborn <petergoulborn@astuntechnology.com>
2014-01-10 11:33:44 +0000

    Change Get Result to Call Function
    Hmmm, seems some invisible formatting changes have occurred.
    
    Anyway, changed Get Result keyword to Call Function since it's really
    only for functions. Added _get_single_result method that this now uses
    and is available for other methods.

--- src/DatabaseLibrary/query.py
+++ src/DatabaseLibrary/query.py
@@ -16,36 +16,36 @@ from robot.api import logger
 
 class Query(object):
     """
-    Query handles all the querying done by the Database Library. 
+    Query handles all the querying done by the Database Library.
     """
 
     def query(self, selectStatement):
         """
-        Uses the input `selectStatement` to query for the values that 
+        Uses the input `selectStatement` to query for the values that
         will be returned as a list of tuples.
-        
-        Tip: Unless you want to log all column values of the specified rows, 
-        try specifying the column names in your select statements 
+
+        Tip: Unless you want to log all column values of the specified rows,
+        try specifying the column names in your select statements
         as much as possible to prevent any unnecessary surprises with schema
-        changes and to easily see what your [] indexing is trying to retrieve 
-        (i.e. instead of `"select * from my_table"`, try 
+        changes and to easily see what your [] indexing is trying to retrieve
+        (i.e. instead of `"select * from my_table"`, try
         `"select id, col_1, col_2 from my_table"`).
-        
+
         For example, given we have a table `person` with the following data:
         | id | first_name  | last_name |
         |  1 | Franz Allan | See       |
-        
+
         When you do the following:
         | @{queryResults} | Query | select * from person |
         | Log Many | @{queryResults} |
-        
+
         You will get the following:
         [1, 'Franz Allan', 'See']
-        
+
         Also, you can do something like this:
         | ${queryResults} | Query | select first_name, last_name from person |
         | Log | ${queryResults[0][1]}, ${queryResults[0][0]} |
-        
+
         And get the following
         See, Franz Allan
         """
@@ -57,50 +57,50 @@ class Query(object):
             return allRows
         finally :
             if cur :
-                self._dbconnection.rollback() 
-                
-    def get_result(self, functionName, *args):
-        """ 
+                self._dbconnection.rollback()
+
+    def call_function(self, functionName, *args):
+        """
         Calls function `functionName` with any extra arguments and returns
         the result. This is not intended for use with functions that return a
         recordset or table (use `query` for those).
-        
+
         For example, calling the standard `version` function in PostgreSQL:
-        | ${ver} | Get Result | version |
+        | ${ver} | Call Function | version |
         | Log | ${ver} |
-        
+
         You will get something like:
         9.2.6
         """
         arguments = ''
         if args:
             arguments = ', '.join(args)
-            
+
         selectStatement = 'SELECT {}({});'.format(functionName, arguments)
-        return self.query(selectStatement)[0][0]
-        
+        return self._get_single_result(selectStatement)
+
 
     def row_count(self, selectStatement):
         """
         Uses the input `selectStatement` to query the database and returns
         the number of rows from the query.
-        
+
         For example, given we have a table `person` with the following data:
         | id | first_name  | last_name |
         |  1 | Franz Allan | See       |
         |  2 | Jerry       | Schneider |
-        
+
         When you do the following:
         | ${rowCount} | Row Count | select * from person |
         | Log | ${rowCount} |
-        
+
         You will get the following:
         2
-        
+
         Also, you can do something like this:
         | ${rowCount} | Row Count | select * from person where id = 2 |
         | Log | ${rowCount} |
-        
+
         And get the following
         1
         """
@@ -113,21 +113,21 @@ class Query(object):
             return rowCount
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def description(self, selectStatement):
         """
         Uses the input `selectStatement` to query a table in the db which
         will be used to determine the description.
-                
+
         For example, given we have a table `person` with the following data:
         | id | first_name  | last_name |
         |  1 | Franz Allan | See       |
-        
+
         When you do the following:
         | @{queryResults} | Description | select * from person |
         | Log Many | @{queryResults} |
-        
+
         You will get the following:
         [Column(name='id', type_code=1043, display_size=None, internal_size=255, precision=None, scale=None, null_ok=None)]
         [Column(name='first_name', type_code=1043, display_size=None, internal_size=255, precision=None, scale=None, null_ok=None)]
@@ -141,14 +141,14 @@ class Query(object):
             return description
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def delete_all_rows_from_table(self, tableName):
         """
         Delete all the rows within a given table.
-        
+
         For example, given we have a table `person` in a database
-        
+
         When you do the following:
         | Delete All Rows From Table | person |
 
@@ -168,13 +168,13 @@ class Query(object):
             self._dbconnection.commit()
         finally :
             if cur :
-                self._dbconnection.rollback() 
+                self._dbconnection.rollback()
 
     def execute_sql_script(self, sqlScriptFileName):
         """
-        Executes the content of the `sqlScriptFileName` as SQL commands. 
-        Useful for setting the database to a known state before running 
-        your tests, or clearing out your test data after running each a 
+        Executes the content of the `sqlScriptFileName` as SQL commands.
+        Useful for setting the database to a known state before running
+        your tests, or clearing out your test data after running each a
         test.
 
         Sample usage :
@@ -185,48 +185,48 @@ class Query(object):
         | Execute Sql Script | ${EXECDIR}${/}resources${/}DDL-teardown.sql |
 
         SQL commands are expected to be delimited by a semi-colon (';').
-        
+
         For example:
         delete from person_employee_table;
         delete from person_table;
-        delete from employee_table; 
-        
+        delete from employee_table;
+
         Also, the last SQL command can optionally omit its trailing semi-colon.
-        
+
         For example:
         delete from person_employee_table;
         delete from person_table;
         delete from employee_table
-        
+
         Given this, that means you can create spread your SQL commands in several
         lines.
-        
+
         For example:
-        delete 
+        delete
           from person_employee_table;
-        delete 
+        delete
           from person_table;
-        delete 
+        delete
           from employee_table
-        
+
         However, lines that starts with a number sign (`#`) are treated as a
         commented line. Thus, none of the contents of that line will be executed.
-        
+
         For example:
         # Delete the bridging table first...
-        delete 
+        delete
           from person_employee_table;
           # ...and then the bridged tables.
-        delete 
+        delete
           from person_table;
-        delete 
+        delete
           from employee_table
         """
         sqlScriptFile = open(sqlScriptFileName)
 
         cur = None
         try:
-            cur = self._dbconnection.cursor()        
+            cur = self._dbconnection.cursor()
             sqlStatement = ''
             for line in sqlScriptFile:
                 line = line.strip()
@@ -234,7 +234,7 @@ class Query(object):
                     continue
                 elif line.startswith('--'):
                     continue
-                
+
                 sqlFragments = line.split(';')
                 if len(sqlFragments) == 1:
                     sqlStatement += line + ' '
@@ -243,21 +243,21 @@ class Query(object):
                         sqlFragment = sqlFragment.strip()
                         if len(sqlFragment) == 0:
                             continue
-                    
+
                         sqlStatement += sqlFragment + ' '
-                        
+
                         self.__execute_sql(cur, sqlStatement)
                         sqlStatement = ''
 
-            sqlStatement = sqlStatement.strip()    
+            sqlStatement = sqlStatement.strip()
             if len(sqlStatement) != 0:
                 self.__execute_sql(cur, sqlStatement)
-                
+
             self._dbconnection.commit()
         finally:
             if cur :
                 self._dbconnection.rollback()
-                
+
     def execute_sql_string(self, sqlString):
         """
         Executes the sqlString as SQL commands.
@@ -282,3 +282,6 @@ class Query(object):
     def __execute_sql(self, cur, sqlStatement):
         logger.debug("Executing : %s" % sqlStatement)
         return cur.execute(sqlStatement)
+
+    def _get_single_result(self, selectStatement):
+        return self.query(selectStatement)[0][0]
